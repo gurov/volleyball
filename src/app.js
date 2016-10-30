4c5897fb66c77495e99440d2136059b02c408049
Pavel Gurov <lucius.gu@ya.ru>
2016-10-30 23:10:43 +0700

    new favicon

--- src/app.js
+++ src/app.js
@@ -6,6 +6,9 @@ import './app.css';
 import angular from 'angular';
 import 'angular-ui-router';
 import 'angular-localforage';
+import 'angular-strap';
+import 'angular-strap/dist/angular-strap.tpl';
+
 import 'lodash';
 
 import {player} from './player/player';
@@ -13,5 +16,6 @@ import {player} from './player/player';
 angular.module('app', [
     'ui.router',
     'LocalForageModule',
+    'mgcrea.ngStrap',
     player.name
 ]);
